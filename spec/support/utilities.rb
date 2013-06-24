57ddc3efb863280105387de96e15d74d5bbb8439
Steve Lovell <sjlovell34@gmail.com>
2013-06-24 16:30:06 -0400

    first commit in os x env

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,12 +1,10 @@
 
 def sign_in(user)
-  describe "sign in" do
-    before { visit signin_path }
+    visit signin_path
     fill_in "Email",    with: user.email
     fill_in "Password", with: user.password
     click_button "Sign In"
     cookies[:remember_token] = user.remember_token
-  end
 end
 
 
