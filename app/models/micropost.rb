3a0a79651cc3b94e5b5fc142936728d83918b1b6
Steve Lovell <sjlovell34@gmail.com>
2013-07-03 16:41:46 -0400

    Add full user news feed

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -5,5 +5,13 @@ class Micropost < ActiveRecord::Base
 
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
-  
+
+  def self.from_users_followed_by(user)
+    # SELECT * FROM MICROPOSTS 
+    # WHERE user.id IN ( SELECT followed_id FROM relationships WHERE follower_id = current_user ) 
+    #               OR user.id = current_user
+
+    followed_user_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
+    where( "user_id IN (#{followed_user_ids}) OR user_id = :user_id", user_id: user.id )
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -50,10 +50,32 @@ describe User do
   end
 
 
+
+
+
   describe "micropost associations" do
     before { @user.save }
     let!(:older_micropost)  { FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago) }
     let!(:newer_micropost) { FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago) }
+
+    describe "status" do
+      let(:unfollowed_post) { FactoryGirl.create(:micropost, user: FactoryGirl.create(:user)) }
+      let(:followed_user) { FactoryGirl.create(:user) }
+
+      before do
+        @user.follow!(followed_user)
+        3.times { followed_user.microposts.create!(content: "Lorem ipsum") }
+      end
+
+      its(:feed) { should include(newer_micropost) }
+      its(:feed) { should include(older_micropost) }
+      its(:feed) { should_not include(unfollowed_post) }
+      its(:feed) do
+        followed_user.microposts.each do |micropost|
+          should include(micropost)
+        end
+      end
+    end
 
     it "should have the right microposts in the right order" do
       @user.microposts.should == [newer_micropost, older_micropost]
